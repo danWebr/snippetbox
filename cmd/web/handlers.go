47661b54f5eb4dfba634417c734e4950607f9de4
danWebr <danilo@weber-familie.de>
2023-07-23 15:32:42 +0200

    Modify routing

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"github.com/julienschmidt/httprouter"
 	"net/http"
 	"snippetbox.danwebr.net/internal/models"
 	"strconv"
@@ -10,10 +11,6 @@ import (
 
 // home handler function
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		app.notFound(w)
-		return
-	}
 
 	snippets, err := app.snippets.Latest()
 	if err != nil {
@@ -30,7 +27,9 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 
 // snippetView handler function
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	params := httprouter.ParamsFromContext(r.Context())
+
+	id, err := strconv.Atoi(params.ByName("id"))
 	if err != nil || id < 1 {
 		app.notFound(w)
 		return
@@ -54,12 +53,12 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 
 // snippetCreate handler function
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		// Respond with 405 status code if POST is not used
-		w.Header().Set("Allow", http.MethodPost)
-		app.clientError(w, http.StatusMethodNotAllowed)
-		return
-	}
+
+	w.Write([]byte("Display the form for creating a new snippet..."))
+}
+
+// snippetCreatePost handler function
+func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
 
 	title := "0 snail"
 	content := "O snail\\nClimb Mount Fuji,\\nBut slowly, slowly!\\n\\n– Kobayashi Issa"
@@ -71,5 +70,5 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
