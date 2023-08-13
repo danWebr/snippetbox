9741e915ab8fc9684af4954f4e92f00968fcdfbc
danWebr <danilo@weber-familie.de>
2023-08-13 15:03:45 +0200

    Add a debug mode

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -17,6 +17,7 @@ import (
 )
 
 type application struct {
+	debug          bool
 	errorLog       *log.Logger
 	infoLog        *log.Logger
 	snippets       models.SnippetModelInterface
@@ -30,6 +31,7 @@ func main() {
 	// Define  and parse command-line flags
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
+	debug := flag.Bool("debug", false, "Enable debug mode")
 	flag.Parse()
 
 	// Create logger for info and error messages
@@ -57,6 +59,7 @@ func main() {
 	sessionManager.Cookie.Secure = true
 
 	app := &application{
+		debug:          *debug,
 		errorLog:       errorLog,
 		infoLog:        infoLog,
 		snippets:       &models.SnippetModel{DB: db},
