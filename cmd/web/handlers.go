4e0ef04645380439bba55acda58e714f8e324549
danWebr <danilo@weber-familie.de>
2023-08-16 21:01:41 +0200

    Implement accountUpdatePasswordPost handler

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -284,6 +284,27 @@ func (app *application) accountUpdatePasswordPost(w http.ResponseWriter, r *http
 		app.render(w, http.StatusUnprocessableEntity, "password.tmpl", data)
 		return
 	}
+
+	userID := app.sessionManager.GetInt(r.Context(), "authenticatedUserID")
+
+	err = app.users.PasswordUpdate(userID, form.CurrentPassword, form.NewPassword)
+	if err != nil {
+		if errors.Is(err, models.ErrInvalidCredentials) {
+			form.AddFieldError("currentPassword", "Current password is incorrect")
+
+			data := app.newTemplateData(r)
+			data.Form = form
+
+			app.render(w, http.StatusUnprocessableEntity, "password.tmpl", data)
+		} else if err != nil {
+			app.serverError(w, err)
+		}
+		return
+	}
+
+	app.sessionManager.Put(r.Context(), "flash", "Your password has been updated!")
+
+	http.Redirect(w, r, "/account/view", http.StatusSeeOther)
 }
 
 func (app *application) about(w http.ResponseWriter, r *http.Request) {
